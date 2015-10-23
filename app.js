a443100e389fb0cdb259c6b50e31e1bb7c62fefb
unknown <zorko.michal@gmail.com>
2015-10-23 07:37:16 +0200

    Api generates data on refresh

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ parking.get('/:id', function(req, res) {
 	console.log("id " + id);
 	var jsonContent = JSON.parse(file);
 	var jsonData = jsonContent.data[id];
-	var incrementer = 0.01
+	var incrementer = 0.03
 	console.log(inc);
 	jsonData.available_lots = free_lots;
 	//jsonData.available_lots = 190;
